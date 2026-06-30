5b558709f529c5bb6a2b28708ef31fe77d5c92af
Al Aiham <ayhem9987@gmail.com>
2026-06-30 12:26:58 +0400

    Added Enrollments Model

--- University System ERD & Models/Program.cs
+++ University System ERD & Models/Program.cs
@@ -1,46 +1,5 @@
-﻿using System.ComponentModel;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-using Microsoft.EntityFrameworkCore;
-namespace University_System_ERD___Models
+﻿namespace University_System_ERD___Models
 {
-
-    
-
-    
-
-    
-
-    
-
-    public class Enrollment
-    {
-        [Key]
-        public int enrollmentId { get; set; }
-
-        [Required]
-        public int studentId { get; set; }
-
-        [ForeignKey("studentId"), Required]
-        public Student student { get; set; }
-
-        [Required]
-        public int courseId { get; set; }
-
-        [ForeignKey("courseId"), Required]
-        public Course course { get; set; }
-
-        [Required]
-        public DateTime enrollmentDate { get; set; }
-
-        [MaxLength(2)]
-        public string finalGrade { get; set; }
-
-        [Required, MaxLength(20), DefaultValue("In Progress")]
-        public string status { get; set; }
-    }
-
-
     internal class Program
     {
         static void Main(string[] args)
