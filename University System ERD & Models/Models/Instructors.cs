cedcd252ae87b61ca1c60d90a76b7c05b45bc030
Al Aiham <ayhem9987@gmail.com>
2026-06-30 12:23:01 +0400

    Added Instructors Model

---
+++ University System ERD & Models/Models/Instructors.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace University_System_ERD___Models.Models
+{
+    [Index(nameof(email), IsUnique = true)]
+    public class Instructor
+    {
+        [Key]
+        public int instructorId { get; set; }
+
+        [Required, MaxLength(100)]
+        public string fullName { get; set; }
+
+        [Required, MaxLength(150)]
+        public string email { get; set; }
+
+        [MaxLength(20)]
+        public string? officeNumber { get; set; }
+
+        [Required]
+        public DateTime hireDate { get; set; }
+
+        [Required, Range(0.1, double.MaxValue)]
+        public decimal salary { get; set; }
+
+        [Required, MaxLength(50)]
+        public string academicTitle { get; set; }
+    }
+}
--- University System ERD & Models/Program.cs
+++ University System ERD & Models/Program.cs
@@ -7,30 +7,7 @@ namespace University_System_ERD___Models
 
     
 
-    [Index(nameof(email), IsUnique = true)]
-    public class Instructor
-    {
-        [Key]
-        public int instructorId { get; set; }
-
-        [Required, MaxLength(100)]
-        public string fullName { get; set; }
-
-        [Required, MaxLength(150)]
-        public string email { get; set; }
-
-        [MaxLength(20)]
-        public string? officeNumber { get; set; }
-
-        [Required]
-        public DateTime hireDate { get; set; }
-
-        [Required, Range(0.1,double.MaxValue)]
-        public decimal salary { get; set; }
-
-        [Required, MaxLength(50)]
-        public string academicTitle { get; set; }
-    }
+    
 
     [Index(nameof(departmentName), IsUnique = true)]
     public class Department
