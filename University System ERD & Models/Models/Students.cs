34e2d7ab883ee3f7f3a352fe68d900bc42973296
Al Aiham <ayhem9987@gmail.com>
2026-06-30 12:22:04 +0400

    Added Models Folder & Student Model

---
+++ University System ERD & Models/Models/Students.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace University_System_ERD___Models.Models
+{
+    [Index(nameof(email), IsUnique = true)]
+    public class Student
+    {
+        [Key]
+        public int studentId { get; set; }
+
+        [Required, StringLength(100)]
+        public string fullName { get; set; }
+
+        [Required, StringLength(150)]
+        public string email { get; set; }
+
+        [StringLength(20)]
+        public string? phoneNumber { get; set; }
+
+        [Required]
+        public DateTime dateOfBirth { get; set; }
+
+        [Required, Range(2000, 2030)]
+        public int enrollmentYear { get; set; }
+
+        [Range(0.0, 4.0), DefaultValue(0.0)]
+        public decimal gpa { get; set; }
+    }
+}
--- University System ERD & Models/Program.cs
+++ University System ERD & Models/Program.cs
@@ -5,30 +5,7 @@ using Microsoft.EntityFrameworkCore;
 namespace University_System_ERD___Models
 {
 
-    [Index(nameof(email), IsUnique = true)]
-    public class Student
-    {
-        [Key]
-        public int studentId {  get; set; }
-
-        [Required, StringLength(100)]        
-        public string fullName { get; set; }
-
-        [Required, StringLength(150)]        
-        public string email { get; set; }
-
-        [StringLength(20)]
-        public string? phoneNumber { get; set; }
-
-        [Required]
-        public DateTime dateOfBirth { get; set; }
-
-        [Required, Range(2000, 2030)]
-        public int enrollmentYear { get; set; }
-
-        [Range(0.0, 4.0), DefaultValue(0.0)]
-        public decimal gpa { get; set; }
-    }
+    
 
     [Index(nameof(email), IsUnique = true)]
     public class Instructor
