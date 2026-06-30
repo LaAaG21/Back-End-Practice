8f4ac392ab91489dda6d5819b1674807f657af8f
Al Aiham <ayhem9987@gmail.com>
2026-06-30 12:09:16 +0400

    Added Course Class

--- University System ERD & Models/Program.cs
+++ University System ERD & Models/Program.cs
@@ -73,8 +73,35 @@ namespace University_System_ERD___Models
 
         [ForeignKey("headInstructorId")]
         public Instructor? headInstructor { get; set; }
+    }
 
+    [Index(nameof(courseCode), IsUnique = true)]
+    public class Course
+    {
+        [Key]
+        public int courseId { get; set; }
 
+        [Required, MaxLength(10)]
+        public string courseCode { get; set; }
+
+        [Required, MaxLength(150)]
+        public string courseTitle { get; set; }
+
+        [Required, Range(1,6)]
+        public int creditHours { get; set; }
+
+        public int? departmentId { get; set; }
+
+        [ForeignKey("departmentId")]
+        public Department? department { get; set; }
+
+        public int? headInstructorId { get; set; }
+
+        [ForeignKey("headInstructorId")]
+        public Instructor? headInstructor { get; set; }
+
+        [Required, MaxLength(20)]
+        public string semesterOffered { get; set; }
     }
 
 
