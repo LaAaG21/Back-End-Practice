4ec43a3840cf9ad7506c081937b08379c92708e4
Al Aiham <ayhem9987@gmail.com>
2026-07-01 10:49:54 +0400

    Added User Class

--- E-Commerce System ERD & Models/Program.cs
+++ E-Commerce System ERD & Models/Program.cs
@@ -1,5 +1,39 @@
-﻿namespace E_Commerce_System_ERD___Models
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace E_Commerce_System_ERD___Models
 {
+
+    [Index(nameof(userName), IsUnique = true), Index(nameof(email), IsUnique = true)]
+    public class User
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int userId { get; set; }
+
+        [Required, MaxLength(50)]
+        public string userName{ get; set; }
+
+        [Required, MaxLength(150)]
+        public string email{ get; set; }
+
+        [Required, MaxLength(256)]
+        public string passwordHash{ get; set; }
+
+        [Required, MaxLength(100)]
+        public string fullName{ get; set; }
+
+        [MaxLength(20)]
+        public string? phoneNumber { get; set; }
+
+        [MaxLength(300)]
+        public string? address { get; set; }
+
+        [Required]
+        public DateTime registrationDate { get; set; }
+
+        public bool isActive { get; set; } = true;
+    }
+
     internal class Program
     {
         static void Main(string[] args)
