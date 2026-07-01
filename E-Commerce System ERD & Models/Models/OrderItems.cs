c55445a7f4ec682697f89a07443297237310aad5
Al Aiham <ayhem9987@gmail.com>
2026-07-01 12:13:55 +0400

    Added OrderItems Model

---
+++ E-Commerce System ERD & Models/Models/OrderItems.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace E_Commerce_System_ERD___Models.Models
+{
+    public class OrderItem
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int orderItemId { get; set; }         // system generated
+
+        public int productId { get; set; }           // foreign key
+
+        [ForeignKey("productId")]
+        public Product product { get; set; }         // navigation property
+
+        public int orderId { get; set; }             // foreign key
+
+        [ForeignKey("orderId")]
+        public Order order { get; set; }             // navigation property
+
+        [Required, Range(1, 999)]
+        public int quantity { get; set; }            // user input
+    }
+}
