ead56c309ed0bcb98023e8b4552c8f220cb4bbe7
Al Aiham <ayhem9987@gmail.com>
2026-07-01 08:39:57 +0400

    Enhancing

--- University System ERD & Models/Models/Students.cs
+++ University System ERD & Models/Models/Students.cs
@@ -1,10 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Text;
 
 namespace University_System_ERD___Models.Models
 {
